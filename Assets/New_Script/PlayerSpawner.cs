c580b1df1a1fd2c8dcdb883bf1c725469e45c9b7
Maximilian-Michael <128221535+Max140804@users.noreply.github.com>
2024-07-27 15:29:14 +0100

    ..

--- Assets/New_Script/PlayerSpawner.cs
+++ Assets/New_Script/PlayerSpawner.cs
@@ -10,73 +10,18 @@ public class PlayerSpawner : MonoBehaviourPunCallbacks
     public Transform spawnPointDown;
     public Transform spawnPointUp;
 
-    private Dictionary<int, GameObject> playerInstances = new Dictionary<int, GameObject>();
-
     private void Start()
     {
-        if (PhotonNetwork.IsConnected)
+        Transform spawnPoint = null;
+        if (PhotonNetwork.IsMasterClient)
         {
-            StartCoroutine(SpawnExistingPlayers());
-        }
-    }
-
-    private IEnumerator SpawnExistingPlayers()
-    {
-        yield return new WaitForSeconds(1f);
-
-        foreach (Player player in PhotonNetwork.PlayerList)
-        {
-            SpawnPlayerFor(player);
-        }
-    }
-
-    private void SpawnPlayerFor(Player player)
-    {
-        Transform spawnPoint = player.IsLocal ? spawnPointDown : spawnPointUp;
-
-        if (player.TagObject == null)
-        {
-            GameObject instance = PhotonNetwork.Instantiate(playerPrefab.name, spawnPoint.position, Quaternion.identity);
-
-            Vector3 originalScale = instance.transform.localScale;
-            instance.transform.SetParent(spawnPoint);
-            instance.transform.localPosition = Vector3.zero;
-            instance.transform.localScale = originalScale;
-
-            player.TagObject = instance;
-            playerInstances[player.ActorNumber] = instance;
-
-            PhotonView photonView = instance.GetComponent<PhotonView>();
-            if (photonView != null && player.IsLocal)
-            {
-                photonView.RPC("SetPlayerName", RpcTarget.AllBuffered, PhotonNetwork.NickName);
-            }
-
-            Debug.Log("Player prefab instantiated and parented: " + instance.name);
+            spawnPoint = spawnPointDown;
         }
         else
         {
-            GameObject instance = (GameObject)player.TagObject;
-            instance.transform.SetParent(spawnPoint);
-            instance.transform.localPosition = Vector3.zero;
-
-            Debug.Log("Player prefab already exists, re-parented: " + instance.name);
+            spawnPoint = spawnPointUp;
         }
-    }
 
-    public override void OnJoinedRoom()
-    {
-        if (!playerInstances.ContainsKey(PhotonNetwork.LocalPlayer.ActorNumber))
-        {
-            SpawnPlayerFor(PhotonNetwork.LocalPlayer);
-        }
-    }
-
-    public override void OnPlayerEnteredRoom(Player newPlayer)
-    {
-        if (!playerInstances.ContainsKey(newPlayer.ActorNumber))
-        {
-            SpawnPlayerFor(newPlayer);
-        }
+        PhotonNetwork.Instantiate(playerPrefab.name, spawnPoint.position, spawnPoint.rotation);
     }
 }
