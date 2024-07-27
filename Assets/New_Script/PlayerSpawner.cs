f544d4f47b3d13744bf1d811cc97c05263e84b7b
Maximilian-Michael <128221535+Max140804@users.noreply.github.com>
2024-07-27 17:16:37 +0100

    .

--- Assets/New_Script/PlayerSpawner.cs
+++ Assets/New_Script/PlayerSpawner.cs
@@ -13,26 +13,49 @@ public class PlayerSpawner : MonoBehaviourPunCallbacks
 
     private void Start()
     {
+        // Spawn existing players at the start
         SpawnAllPlayers();
+    }
+
+    public override void OnPlayerEnteredRoom(Player newPlayer)
+    {
+        // Spawn the newly joined player
+        SpawnPlayer(newPlayer);
+    }
+
+    public override void OnPlayerLeftRoom(Player otherPlayer)
+    {
+        // Remove the player who left
+        if (spawnedPlayers.ContainsKey(otherPlayer.ActorNumber))
+        {
+            Destroy(spawnedPlayers[otherPlayer.ActorNumber]);
+            spawnedPlayers.Remove(otherPlayer.ActorNumber);
+        }
     }
 
     private void SpawnAllPlayers()
     {
         foreach (Player player in PhotonNetwork.PlayerList)
         {
-            Transform spawnPoint = player == PhotonNetwork.LocalPlayer ? spawnPointDown : spawnPointUp;
-
-            if (spawnedPlayers.ContainsKey(player.ActorNumber))
-            {
-                continue;
-            }
-
-            GameObject playerToSpawn = PhotonNetwork.Instantiate(playerPrefab.name, spawnPoint.position, spawnPoint.rotation);
-            playerToSpawn.transform.SetParent(spawnPoint, false);
-            playerToSpawn.transform.localPosition = Vector3.zero;
-            playerToSpawn.transform.localScale = playerPrefab.transform.localScale;
-
-            spawnedPlayers[player.ActorNumber] = playerToSpawn;
+            SpawnPlayer(player);
         }
+    }
+
+    private void SpawnPlayer(Player player)
+    {
+        if (spawnedPlayers.ContainsKey(player.ActorNumber))
+        {
+            // Player is already spawned
+            return;
+        }
+
+        Transform spawnPoint = player == PhotonNetwork.LocalPlayer ? spawnPointDown : spawnPointUp;
+
+        GameObject playerToSpawn = PhotonNetwork.Instantiate(playerPrefab.name, spawnPoint.position, spawnPoint.rotation);
+        playerToSpawn.transform.SetParent(spawnPoint, false);
+        playerToSpawn.transform.localPosition = Vector3.zero;
+        playerToSpawn.transform.localScale = playerPrefab.transform.localScale;
+
+        spawnedPlayers[player.ActorNumber] = playerToSpawn;
     }
 }
